406e3729aa1e73144843e6ee8270109bd1730f78
jubilee13 <bileesantos13@gmail.com>
2026-01-20 23:34:27 +0800

    initialized springboot project

---
+++ src/test/java/com/io/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
